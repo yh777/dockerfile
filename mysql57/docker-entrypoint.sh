fddcfc7a16923b083e52c875bd414d7e15c0c86a
yh777 <21872017@qq.com>
2018-08-07 18:18:09 +0800

    fix

--- mysql57/docker-entrypoint.sh
+++ mysql57/docker-entrypoint.sh
@@ -5,5 +5,5 @@ then
 else
     /opt/mysql57/bin/mysqld --initialize-insecure --basedir=/opt/mysql57/ --datadir=/opt/mysql57/var --user=mysql
     /opt/mysql57/bin/mysqld_safe --user=mysql
-    /opt/mysql57/bin/mysql -e "use mysql;  update user set authentication_string = password('jZ3p14Q03gyB16a'),password_expired = 'N',password_last_changed = now() where user = 'root'; flush privileges"
+    /opt/mysql57/bin/mysql -e "use mysql; update user set authentication_string = password('jZ3p14Q03gyB16a'),password_expired = 'N',password_last_changed = now() where user = 'root'; grant all privileges on *.* to root@'%' identified by "jZ3p14Q03gyB16a" with grant option; flush privileges;"
 fi
